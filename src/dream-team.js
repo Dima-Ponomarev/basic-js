c4e9efea484a7d8da29ec25ae8bebe5646220d76
Dima Ponomarev <kikkyow18@gmail.com>
2021-02-25 11:54:29 +0400

    feat:implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if (Array.isArray(members)){
+    let teamArr = [];
+    members.forEach(member => {
+      if (typeof member === 'string'){
+        for (let i = 0; i < member.length; i++){
+          if (member[i] !== ' '){
+            teamArr.push(member[i].toUpperCase());
+            break;
+          }
+        }
+      }
+    });
+    return teamArr.sort().join('');
+  } else {
+    return false;
+  }
 };
