2206bee300c2b5f1cff63d48d971536b08c81b6d
Dima Ponomarev <kikkyow18@gmail.com>
2021-03-07 17:04:51 +0400

    feat:implement repeater function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chainArray: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainArray.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArray.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (Number.isInteger(position)){
+      if (position <= this.chainArray.length){
+        this.chainArray.splice(position - 1, 1);
+      } else {
+        this.chainArray = [];
+        throw new RangeError('Parameter is out of range');
+
+      }
+    } else {
+      this.chainArray = [];
+      throw new TypeError("Wrong parameter type") 
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArray.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chainString = '';
+    if (this.chainArray.length === 1){
+      chainString += `( ${this.chainArray[0]} )`
+    } else {
+      this.chainArray.forEach((item, index) => {
+        if (index === 0){
+            chainString += `( ${item} )~`;
+        } else if (index === this.chainArray.length - 1){
+            chainString += `~( ${item} )`;
+        } else {
+            chainString += `~( ${item} )~`
+        }
+      });
+    }  
+   
+    this.chainArray = [];
+    return chainString
   }
 };
 
